695f1dbf77f6072e1c44574c84315f616ac86a39
bmerckling01 <bmerckling01@PC-0287.csip.in>
2024-03-28 09:51:14 +0100

    v1 (main avec tableau pour les coins) A faire : associer les murs entre eux pour faire des pièces carrés

--- src/main/java/fr/insa/eich/projets2/ProjetS2.java
+++ src/main/java/fr/insa/eich/projets2/ProjetS2.java
@@ -3,6 +3,8 @@
  */
 
 package fr.insa.eich.projets2;
+import java.io.*; 
+import java.util.*;
 
 /**
  *
@@ -11,6 +13,24 @@ package fr.insa.eich.projets2;
 public class ProjetS2 {
 
     public static void main(String[] args) {
-        
+    System.out.println(" Regles pour la saisie : 1. Chaque piece fait 4 murs vous devez écrire les deux coins de chaque mur ");
+    System.out.println(" 2. Meme si deux murs ont des coins communs, il faut reecrire les coordonnees ");
+    System.out.println(" Premierement, donnez les coordonées (x) des coins de vos murs puis, quand c'est fini ecrivez fin " );
+    String [][] Couple_coins  = new String [100][2];
+    int n=0;
+   String tabulation = (Lire.S()); 
+   while (tabulation != "fin") {
+    Couple_coins [n][1] = tabulation;
+    n=n+1;
+   }
+   System.out.println(" Maintenant, donnez les coordonées y associees aux x des coins de vos murs puis, quand c'est fini ecrivez fin " );
+   System.out.println( " Indication : x ecrit en premier et y ecrit en premier = un coin ");
+   while (tabulation != "fin") {
+    Couple_coins [n][2] = tabulation;
+    n=n+1;
+   }
+ System.out.println(Couple_coins);
+   
+   
     }
 }
