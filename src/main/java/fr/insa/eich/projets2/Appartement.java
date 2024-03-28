2d41bb16608124fad1df840e0df07b0dca8a8cae
arbre <arbre@172.17.113.117>
2024-03-28 09:36:33 +0100

    TP3

---
+++ src/main/java/fr/insa/eich/projets2/Appartement.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.eich.projets2;
+import java.util.*;
+
+/**
+ *
+ * @author arbre
+ */
+public class Appartement {
+    private int id;
+    private int niveau ;
+    List pièces = new ArrayList();
+    
+    
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
