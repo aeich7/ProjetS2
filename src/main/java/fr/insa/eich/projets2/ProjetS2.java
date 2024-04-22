823e096f0f2ed19072509b2ecf3752d5ce0475ee
bruno <bruno@172.17.70.240>
2024-04-22 14:01:52 +0200

    v1.2 améliorée (matrice de revêtements installée et liste créée pour choix revet)

--- src/main/java/fr/insa/eich/projets2/ProjetS2.java
+++ src/main/java/fr/insa/eich/projets2/ProjetS2.java
@@ -3,7 +3,7 @@
  */
 
 package fr.insa.eich.projets2;
-import static fr.insa.eich.projets2.Revetements.Tableau;
+import static fr.insa.eich.projets2.Revetements.*;
 import java.io.*; 
 import java.util.*;
 
@@ -28,7 +28,35 @@ public class ProjetS2 {
         // Exemple d'utilisation de la méthode
         String[][] contenu = new String[19][6];  // initialisation d'une matrice pour y mettre les données récupérées dans la méthode Tableau
         String[][] resultat = Tableau("Tableau.txt", contenu); // création d'une autre matrice pour y mettre celle créé dans la méthode Tableau pour pouvoir l'afficher
-         afficherMatrice(resultat);
+        
+        
+        System.out.println(" Voici les revêtements possibles : ");
+        afficherMatrice(resultat);
+        
+        String [][]results = mur(resultat); //création d'une matrice pour y mettre les revêtements qui peuvent aller avec mur
+        // results prend la matrice resultat et n'écrit que les rvet utiles pour un mur 
+        System.out.println();
+         
+        System.out.println(" Voici les revêtements que vous pouvez utiliser pour les murs : ");
+        afficherMatrice(results);
+        
+        String [][]results1 = sol(resultat); //création d'une matrice pour y mettre les revêtements qui peuvent aller avec mur
+        // results prend la matrice resultat et n'écrit que les rvet utiles pour un mur 
+        System.out.println();
+         
+        System.out.println(" Voici les revêtements que vous pouvez utiliser pour les sols : ");
+        afficherMatrice(results1);
+        
+         String [][]results2 = plafond(resultat); //création d'une matrice pour y mettre les revêtements qui peuvent aller avec mur
+        // results prend la matrice resultat et n'écrit que les rvet utiles pour un mur 
+        System.out.println();
+         
+        System.out.println(" Voici les revêtements que vous pouvez utiliser pour les plafonds : ");
+        afficherMatrice(results2);
+        
+        
+     
+      
      }
 }
    
--- src/main/java/fr/insa/eich/projets2/Revetements.java
+++ src/main/java/fr/insa/eich/projets2/Revetements.java
@@ -77,10 +77,89 @@ public class Revetements {
         return Contenu;
     }
 
-  public static void main (){
+  /*public static void main (){
       String[][] contenu = new String[19][6];  // initialisation d'une matrice pour y mettre les données récupérées dans la méthode Tableau
-      String[][] resultat = Tableau("Tableau.txt", contenu); // création d'une autre matrice pour y mettre celle créé dans la méthode Tableau pour pouvoir l'afficher
-  }  
+      String[][] resultat = Tableau("Tableau.txt", contenu); // création d'une autre matrice pour y mettre celle créé dans la méthode Tableau pour pouvoir l'afficher   
+  }
+    */
+  public static String[][]mur(String Contents [][]) {
+        int i,j, k=0,l=0 ;
+        
+        for(i=0;i<19;i++) {                 // comptage du nombre de lignes nécessaires au tableau de revet de mur
+            if (Contents[i][2].equalsIgnoreCase("1")){
+               k=k+1; 
+            }
+        }
+        String [][]associemur= new String [k][3]; // on affiche les id, les désignations et le prix à l'unité pour permettre au client de choisir le revêtement qu'il veut 
+        // On affiche tout ça dans un tableau (aka revet mur)
+        // On pourra s'en servir dès que user créé un mur en lui demandant direct le revêtement qu'il veut utiliser dessus
+        for(i=0;i<19;i++){     
+                if (Contents[i][2].equalsIgnoreCase("1")){
+                    for (j=0;j<2;j++){
+                        associemur[l][j]= Contents [i][j] ;
+                        associemur[l][2]= Contents [i][5];
+                    }
+                 l=l+1;
+                }
 }
+        return associemur  ;
+  }
+  
+  public static String[][]sol(String Contents [][]) {
+        int i,j, k=0,l=0 ;
+        
+        for(i=0;i<19;i++) {                 // comptage du nombre de lignes nécessaires au tableau de revet de sol
+            if (Contents[i][3].equalsIgnoreCase("1")){
+               k=k+1; 
+            }
+        }
+        String [][]associemur= new String [k][3]; // on affiche les id, les désignations et le prix à l'unité pour permettre au client de choisir le revêtement qu'il veut 
+        // On affiche tout ça dans un tableau (aka revet sol)
+        // On pourra s'en servir dès que user créé une pièce en lui demandant direct le revêtement qu'il veut utiliser sur le sol
+        for(i=0;i<19;i++){     
+                if (Contents[i][3].equalsIgnoreCase("1")){
+                    for (j=0;j<2;j++){
+                        associemur[l][j]= Contents [i][j] ;
+                        associemur[l][2]= Contents [i][5];
+                    }
+                 l=l+1;
+                }
+}
+        return associemur  ;
+  }
+  
+  public static String[][]plafond(String Contents [][]) {
+        int i,j, k=0,l=0 ;
+        
+        for(i=0;i<19;i++) {                 // comptage du nombre de lignes nécessaires au tableau de revet de plafond
+            if (Contents[i][4].equalsIgnoreCase("1")){
+               k=k+1; 
+            }
+        }
+        String [][]associemur= new String [k][3]; // on affiche les id, les désignations et le prix à l'unité pour permettre au client de choisir le revêtement qu'il veut 
+        // On affiche tout ça dans un tableau (aka revet plafond)
+        // On pourra s'en servir dès que user créé une pièce en lui demandant direct le revêtement qu'il veut utiliser au plafond
+        for(i=0;i<19;i++){     
+                if (Contents[i][4].equalsIgnoreCase("1")){
+                    for (j=0;j<2;j++){
+                        associemur[l][j]= Contents [i][j] ;
+                        associemur[l][2]= Contents [i][5];
+                    }
+                 l=l+1;
+                }
+}
+        return associemur  ;
+  }
+
+    @Override
+    public String toString() {
+        return "Revetements{" + "id=" + id + ", designation=" + designation + ", pourMur=" + pourMur + ", pourSol=" + pourSol + ", pourPlafond=" + pourPlafond + ", Prixunitaire=" + Prixunitaire + '}';
+    }
+  
+
+ /*int ide = Integer.parseInt(resultat[i][0]); // manipulation pour passer de string à int 
+            double prx = Double.parseDouble(resultat[i][5]); // manipulation pour passer de string à double 
+     */ 
 
 
+}
